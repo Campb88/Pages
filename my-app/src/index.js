4a33b28755f54c268b86863cca64cc095f29ec0e
Campb88 <108759113+Campb88@users.noreply.github.com>
2023-11-25 11:58:23 -0800

    Update index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'my-app/src/index.css';
-import App from './App';
+import App from 'my-app/src/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
