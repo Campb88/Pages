e14ade3dc6ec9f72d4087562c450038f7b36d714
Campb88 <108759113+Campb88@users.noreply.github.com>
2023-11-25 12:39:58 -0800

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
-import logo from 'logo.svg';
-import 'App.css';
+import React from 'react';
+import logo from './logo.svg'; // Adjust the path based on your file structure
+import './App.css';
 
 function App() {
   return (
